1db52ad4ab3d0ae6b1675a5ad6dbf0d5495af56a
hieutran29 <hieu.xteen28@gmail.com>
2021-04-03 10:56:55 +0700

    DigitalVideoDisc.java: remove newDVD()

--- Lab06/AimsProject/src/hust/soict/globalict/aims/media/DigitalVideoDisc.java
+++ Lab06/AimsProject/src/hust/soict/globalict/aims/media/DigitalVideoDisc.java
@@ -3,8 +3,6 @@ package hust.soict.globalict.aims.media;
  * @author HieuTran
  */
 
-import java.util.Scanner;
-
 
 public class DigitalVideoDisc extends Media {
 	private String director = "";
@@ -38,61 +36,6 @@ public class DigitalVideoDisc extends Media {
 	}
 	public int length() {
 		return this.length;
-	}
-	
-	public static DigitalVideoDisc newDVD() {
-		String title = "", category = "", director = "";
-		int length = 0;
-		float cost = 0.0f;
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter Title: ");
-		title = scanner.nextLine();
-		
-		System.out.print("Enter Category: ");
-		category = scanner.nextLine();
-		
-		System.out.print("Enter Director: ");
-		director = scanner.nextLine();
-		
-		int choice = 0;
-		
-		do {
-			System.out.print("Enter Length: ");
-			length = scanner.nextInt();
-			if(length < 0) {
-				System.out.println("****You cannot input a negative length****");
-				System.out.println("1. Reinput, or");
-				System.out.println("0. Exit, and Length = 0");
-				System.out.println("*****************************************");
-				System.out.print("Option: ");
-				choice = scanner.nextInt();
-				if(choice == 0) {
-					length = 0;
-				}
-			}
-			
-		} while(choice != 0 || length < 0);
-		
-		do {
-			System.out.print("Enter Cost: ");
-			cost = scanner.nextFloat();
-			if(cost < 0) {
-				System.out.println("****You cannot input a negative cost****");
-				System.out.println("1. Reinput, or");
-				System.out.println("0. Exit, and Cost = 0");
-				System.out.println("*****************************************");
-				System.out.print("Option: ");
-				choice = scanner.nextInt();
-				if(choice == 0) {
-					cost = 0f;
-				}
-			}
-		} while(choice != 0 || cost < 0);
-
-		scanner.close();
-		
-		return new DigitalVideoDisc(title, category, director, length, cost);
 	}
 	
 	public void playDVD() {
