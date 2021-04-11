e4140eb5461cbf5d1c2b49cc86526e1e981dbe9d
hieutran29 <hieu.xteen28@gmail.com>
2021-04-11 11:21:39 +0700

    added Thread to Aims class

--- Lab07/AimsProject/src/hust/soict/globalict/aims/Aims.java
+++ Lab07/AimsProject/src/hust/soict/globalict/aims/Aims.java
@@ -9,6 +9,9 @@ import hust.soict.globalict.aims.controller.MenuController;
 
 public class Aims {
 	public static void main(String[] args) {
+		MemoryDaemon memoryDaemon = new MemoryDaemon();
+		Thread thread = new Thread(memoryDaemon);
+		thread.start();
 		MenuController menuController = new MenuController();
 		menuController.start();
 	}
