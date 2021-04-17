2e07225712091d4ee47016e940026bbdcd436e0d
hieutran29 <hieu.xteen28@gmail.com>
2021-04-17 11:39:30 +0700

    remove DVDUtils and update DVDTest

--- Lab08/AimsProject/src/hust/soict/globalict/test/utils/DVDTest.java
+++ Lab08/AimsProject/src/hust/soict/globalict/test/utils/DVDTest.java
@@ -1,16 +1,17 @@
 package hust.soict.globalict.test.utils;
 import hust.soict.globalict.aims.model.disc.DigitalVideoDisc;
-import hust.soict.globalict.aims.utils.DVDUtils;
+import hust.soict.globalict.aims.model.media.Media;
+import hust.soict.globalict.aims.utils.MediaUtils;
 
 public class DVDTest {
 	public static void main(String[] args) {
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("AAAA", "aaaa", "aaaa", 1, 5.6f);
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("BBBB", "bbbb", "bbbb", 2, 5.3f);
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc("CCCC", "cccc", "cccc", 3, 5f);
-		DigitalVideoDisc dvd4 = new DigitalVideoDisc("DDDD", "dddd", "dddd", 4, 7.1f);
-		DigitalVideoDisc dvd5 = new DigitalVideoDisc("EEEE", "eeee", "eeee", 5, 3.3f);
+		Media dvd1 = new DigitalVideoDisc("AAAA", "aaaa", "aaaa", 1, 5.6f);
+		Media dvd2 = new DigitalVideoDisc("BBBB", "bbbb", "bbbb", 2, 5.3f);
+		Media dvd3 = new DigitalVideoDisc("CCCC", "cccc", "cccc", 3, 5f);
+		Media dvd4 = new DigitalVideoDisc("DDDD", "dddd", "dddd", 4, 7.1f);
+		Media dvd5 = new DigitalVideoDisc("EEEE", "eeee", "eeee", 5, 3.3f);
 		
-		int compareByCost = DVDUtils.compareByCost(dvd1, dvd2);
+		int compareByCost = MediaUtils.compareByCost(dvd1, dvd2);
 		if(compareByCost == 0) {
 			System.out.println("dvd1.cost() == dvd2.cost()");
 		}
@@ -22,7 +23,7 @@ public class DVDTest {
 		}
 		System.out.println();
 		
-		int compareByTitle = DVDUtils.compareByTitle(dvd1, dvd2);
+		int compareByTitle = MediaUtils.compareByTitle(dvd1, dvd2);
 		if(compareByTitle == 0) {
 			System.out.println("dvd1.title() == dvd2.title()");
 		}
@@ -34,7 +35,7 @@ public class DVDTest {
 		}
 		System.out.println();
 		
-		DigitalVideoDisc[] sorted = DVDUtils.sortByCost(new DigitalVideoDisc[]
+		Media[] sorted = MediaUtils.sortByCost(new Media[]
 									{dvd1, dvd2, dvd3, dvd4, dvd5});
 		System.out.println("Sort by cost:");
 		for(int i = 0; i < sorted.length; i++) {
@@ -43,7 +44,7 @@ public class DVDTest {
 		}
 		System.out.println();
 		
-		sorted = DVDUtils.sortByTitle(new DigitalVideoDisc[]
+		sorted = MediaUtils.sortByTitle(new Media[]
 				{dvd1, dvd2, dvd3, dvd4, dvd5});
 		System.out.println("Sort by title:");
 		for(int i = 0; i < sorted.length; i++) {
