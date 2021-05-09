2cda4ad8566775c3b22dfd870463218eab720798
hieutran29 <hieu.xteen28@gmail.com>
2021-05-09 16:10:24 +0700

    make StoreDB and CartDB static

--- Lab09/AimsProject/src/hust/soict/globalict/aims/controller/Controller.java
+++ Lab09/AimsProject/src/hust/soict/globalict/aims/controller/Controller.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 import hust.soict.globalict.aims.data.CartDB;
 import hust.soict.globalict.aims.data.StoreDB;
+import hust.soict.globalict.aims.model.cart.Cart;
+import hust.soict.globalict.aims.model.store.Store;
 
 public abstract class Controller {
     protected static Scanner scanner = new Scanner(System.in);
-    protected static StoreDB storeDB = new StoreDB();
-    protected static CartDB cartDB = new CartDB();
 }
--- Lab09/AimsProject/src/hust/soict/globalict/aims/data/CartDB.java
+++ Lab09/AimsProject/src/hust/soict/globalict/aims/data/CartDB.java
@@ -3,7 +3,7 @@ package hust.soict.globalict.aims.data;
 import hust.soict.globalict.aims.model.cart.Cart;
 
 public class CartDB {
-    public Cart cart = new Cart();
+    public static Cart cart = new Cart();
 
     public CartDB() {
         super();
